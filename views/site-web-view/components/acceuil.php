5aa906ef6e0c24792bc536f06f2e7c4ad1150abb
Hugo Clavinas <helc85.info@gmail.com>
2021-12-30 20:26:34 +0100

    creation du header de l'acceuil mais pas encore fini

--- views/site-web-view/components/acceuil.php
+++ views/site-web-view/components/acceuil.php
@@ -2,7 +2,35 @@
 
 <?php ob_start(); ?>
 
-<h1>home</h1>
+<header>
+    <div class="header_head">
+        <ul class="social_network">
+            <!-- <li><a href=""><i class="fab fa-facebook-f"></i></a></li> -->
+        </ul>
+
+        <ul class="menu">
+            <li><a href=""><i class="fas fa-bars"></i> &nbsp Menu</a>
+
+                <ul>
+                    <li></li>
+                    <li><a href="">lien</a></li>
+                    <li><a href="">lien</a></li>
+                    <li><a href="">lien</a></li>
+                    <li><a href="">lien</a></li>
+                </ul>
+
+            </li>
+
+        </ul>
+    </div>
+
+    <div class="header_body">
+        <h1>WAWMOMO</h1>
+        <h4>Lorem ipsum dolor sit amet.</h4>
+    </div>
+
+    <i class="fleche fas fa-long-arrow-alt-down"></i>
+</header>
 
 <?php $content = ob_get_clean(); ?>
 
